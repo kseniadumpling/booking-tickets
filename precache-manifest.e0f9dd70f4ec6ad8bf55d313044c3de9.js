a76288f8e03db1e6505bfe717b3641141d28b8f4
kseniadumpling <ksenia.dumpling@gmail.com>
2019-09-01 14:25:04 +0300

    Updates

---
+++ precache-manifest.e0f9dd70f4ec6ad8bf55d313044c3de9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "545eb71ed286a02e59bc2383621e4fa1",
+    "url": "/booking-tickets/index.html"
+  },
+  {
+    "revision": "fe054cf7dce32152e211",
+    "url": "/booking-tickets/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "85a9113a0a5ef2827d72",
+    "url": "/booking-tickets/static/css/main.b04db430.chunk.css"
+  },
+  {
+    "revision": "fe054cf7dce32152e211",
+    "url": "/booking-tickets/static/js/2.c4e96db6.chunk.js"
+  },
+  {
+    "revision": "85a9113a0a5ef2827d72",
+    "url": "/booking-tickets/static/js/main.2bbddb84.chunk.js"
+  },
+  {
+    "revision": "04c37a43306aa88a42f8",
+    "url": "/booking-tickets/static/js/runtime~main.b7a2ac69.js"
+  }
+]);
